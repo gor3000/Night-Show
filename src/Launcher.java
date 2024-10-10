5da1221849f87ac328c19387d4f6f2fe36d7ef6f
gor3000 <aleveri3000@gmail.com>
2024-10-11 00:40:10 +0200

    Implement mod function

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,24 +3,33 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        int a=5;
-        int b=3;
-        System.out.println(add(a,b));
-        System.out.println(sub(a,b));
-        System.out.println(mul(a,b));
-        System.out.println(div(a,b));
+        int a = 5;
+        int b = 3;
+        System.out.println(add(a, b));
+        System.out.println(sub(a, b));
+        System.out.println(mul(a, b));
+        System.out.println(div(a, b));
+        System.out.println(mod(a, b));
 
     }
+
     private static int add(int a, int b) {
         return a + b;
     }
+
     private static int sub(int a, int b) {
         return a - b;
     }
+
     private static int mul(int a, int b) {
         return a * b;
-}
+    }
+
     private static int div(int a, int b) {
         return a / b;
+    }
+
+    private static int mod(int a, int b) {
+        return a % b;
     }
 }
